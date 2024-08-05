156874e9fc8b23154073137ea4fc025c59bcdcc3
danielnguyen982301 <danielnguyen982301@gmail.com>
2024-08-05 18:49:37 +0700

    Removed regex for password validation

--- validations/authSchema.ts
+++ validations/authSchema.ts
@@ -3,8 +3,6 @@ import Joi from "joi";
 export const authSchema = {
   body: Joi.object({
     email: Joi.string().email({ allowFullyQualified: true }).required(),
-    password: Joi.string()
-      .regex(/^[\w]+$/)
-      .required(),
+    password: Joi.string().required(),
   }),
 };
